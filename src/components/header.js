7a3c4bbfd301f4424f2d90721fdf3ee293e10fa2
Sahar Levy <sahar@nogalesfundmgmt.com>
2023-08-16 14:29:01 -0700

    add DOM elements that hold the date, title, and temperature

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-const Header = (title, date, temp) => {
+
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -11,9 +11,29 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
 
-const headerAppender = (selector) => {
+const Header = (title, date, temp) => {
+  const header = document.createElement('div');
+  const dateElement = document.createElement('span');
+  const titleElement = document.createElement('h1');
+  const tempElement = document.createElement('span');
+  
+  header.classList.add('header');
+  dateElement.classList.add('date');
+  tempElement.classList.add('temp');
+
+  dateElement.textContent = date;
+  titleElement.textContent = title;
+  tempElement.textContent = temp;
+
+  header.appendChild(dateElement);
+  header.appendChild(titleElement);
+  header.appendChild(tempElement);
+
+  return header;
+}
+Header();
+
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
@@ -26,6 +46,12 @@ const headerAppender = (selector) => {
   // We are taking care of passing in the correct selector on line 16,
   // so all that you need to do is pass it into the querySelector method
   // for the tests to work!
+const headerAppender = (selector) => {
+  const headerElement = Header('BloomTech Newspaper', 'August 16, 2023', '25°C');
+
+  const targetElement = document.querySelector(selector);
+
+  targetElement.appendChild(headerElement);
 }
 
 export { Header, headerAppender }
